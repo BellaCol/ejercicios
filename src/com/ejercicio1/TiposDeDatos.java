492204e7c3b3495a98a4a626388c99ced9b32990
Bella Kharina <kayjoseg5@gmail.com>
2023-05-17 14:16:02 -0300

    Initial commit

---
+++ src/com/ejercicio1/TiposDeDatos.java
@@ -0,0 +1,45 @@
+package com.ejercicio1;
+
+
+
+public class TiposDeDatos {
+
+    /**
+     * Ejercicio N° 1 Tipos de Datos
+     * @param args
+     */
+
+    public static void main(String[] args) {
+
+        //1.- Numéricos
+        //1.1 Enteros
+        byte variable1= 4;
+        short variable2= 53;
+        int variable3= 6;
+        long variable4= 899900887 ;
+        //1.2 Decimales
+        float variable5= 7.5f;
+        double variable6= 84.5657896d;
+        //1.3 Hexadecimales
+
+        //2.- Booleans
+        boolean variable7= true;
+        boolean variable8= false;
+
+        //3.-Texto
+
+        String variable9="Hola Mundo...!";
+        char variable10= '0';
+
+        System.out.println("Variable tipo numérica byte: " + variable1);
+        System.out.println("Variable tipo numérica short: " + variable2);
+        System.out.println("Variable tipo numérica int: " + variable3);
+        System.out.println("Variable tipo numérica long: " + variable4);
+        System.out.println("Variable tipo decimal float: " + variable5);
+        System.out.println("Variable tipo decimal double: " + variable6);
+        System.out.println("Variable tipo booleans: " + variable7);
+        System.out.println("Variable tipo booleans: " + variable8);
+        System.out.println("Variable tipo String: " + variable9);
+        System.out.println("Variable tipo chart: " + variable10);
+    }
+}
